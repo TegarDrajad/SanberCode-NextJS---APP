a252892555ed508abaf1e466f33874a062745e09
TegarDrajad <tegardsv@gmail.com>
2025-02-21 16:55:09 +0700

    Tugas hari-10 Data Fetching (App Router)

---
+++ app/notes/client/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useEffect, useState } from "react";
+
+export default function Notes() {
+
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+      fetch('https://service.pace-unv.cloud/api/notes')
+      .then((res) => res.json())
+      .then((data) => setData(data?.data || []))
+      .finally(() => setLoading(false))
+    }, [])
+
+    if (loading) {
+        return <div>
+            Loading...
+        </div>
+    }
+    return (
+        <>
+            <ul>
+                {data?.map((el) => (
+                    <li key={el.id}>{el.title}</li>
+                )) || []}
+            </ul>
+        </>
+    )
+}
